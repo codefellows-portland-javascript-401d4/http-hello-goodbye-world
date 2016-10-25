205d81580205e391d26249313e42db15f4513d7f
Nathan P. Keene <npk@txtincorporated.com>
2016-10-25 00:33:39 -0700

    Final attempt to pass travis-ci

--- tests/reqHandlers.test.js
+++ tests/reqHandlers.test.js
@@ -1,7 +1,9 @@
-// var figlet = require('figlet-cli');
+var figlet = require('figlet-cli');
+var reqHandlers = require('../lib/requestHandlers');
 
 describe('Tests TBA', () => {
   it('does all that and then some!', () => {
+    reqHandlers.hello();
     assert.equal((1+1), 2);
   });
 });
