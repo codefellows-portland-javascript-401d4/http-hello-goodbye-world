90925a31aa51630fafe912c1cf387c0e320752d1
elhendricks <elhendricks@gmail.com>
2016-10-24 17:12:13 -0700

    wrote an http server that responds based on the path given.  If given / will say hi otherwise will respond based on the request url.

---
+++ server.js
@@ -0,0 +1,40 @@
+const http = require('http');
+const fs = require('fs');
+const hello = fs.createReadStream('hello.html');
+const goodbye = fs.createReadStream('goodbye.html');
+const cowsay = require('cowsay');
+
+
+const server = http.createServer((request, response) => {
+    console.log(request.url);
+    response.statusCode = 200;
+    if (request.url === '/') {
+        var hiMsg = cowsay.say({
+            text: "Hi!!!!",
+            e: "00",
+            T: "U"
+        })
+        response.write(hiMsg);
+        response.end();
+    } else {
+        var text = "I'm a cow and I think about " + request.url.replace('/', '')
+        var byeMsg = cowsay.say({
+            text: text,
+            e: "Oo",
+            T: "U"
+        });
+       
+        response.write(byeMsg)
+        response.end();
+    }
+
+});
+
+
+
+const port = 3000;
+
+server.listen(port, err => {
+    if (err) {console.log('error: ', err)}
+    else {console.log('server listening on port: ', port)}
+});
