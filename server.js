368d5d457f6ad9c1253dd321b8d39b07e17d596d
Tyler Smith <tyler.smith6891@gmail.com>
2016-10-26 14:38:20 -0700

    all routes working and removed extraneous code

--- server.js
+++ server.js
@@ -2,30 +2,21 @@
 
 const http = require('http');
 const fs = require('fs');
-const indexHtml = fs.createReadStream('index.html');
 
 const server = http.createServer((req, res) => {
     console.log(req.url);
     res.statusCode = 200;
     if (req.url === '/') {
-        // fs.readFile('index.html', 'utf-8', (err, data) => {
-        //     if (err) {
-        //         res.statusCode = 500;
-        //         res.end('page read fail');
-        //     } else {
-        //         res.write(data);
-        //         res.end();
-        //     }
-        // });
+        fs.readFile('index.html', 'utf-8', (err, data) => {
+            if (err) {
+                res.statusCode = 500;
+                res.end('page read fail');
+            } else {
+                res.write(data);
+                res.end();
+            }
+        });
 
-        // indexHtml.on('data', data => {
-        //     res.write(data);
-        // });
-        // indexHtml.on('end', () => {
-        //     res.end();
-        // });
-
-        indexHtml.pipe(res);
     } else if (req.url === '/this') {
         res.write(`hello world, ${req.url} is not the droid you're looking for?`);
         res.end();
@@ -35,7 +26,7 @@ const server = http.createServer((req, res) => {
     }
 });
 
-const port = 55555;
+const port = 8080;
 server.listen(port, err => {
     if (err) console.log('Error', err);
     else console.log(`http server listening on port ${port}`);
