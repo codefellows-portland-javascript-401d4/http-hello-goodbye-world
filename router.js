b65e46086930d4bd0cbb4732a83ff6f2e817a256
Gloria Guy <totallygloria@gmail.com>
2016-10-26 13:49:02 -0700

    Added logic to check the path and if it's one of three special routes, to change the default behavior, otherwise just display the route hit in figlet text on the page. Special routes are /, /hello, and /bye.

--- router.js
+++ router.js
@@ -4,12 +4,23 @@ const figlet = require('figlet');
 function route(pathname, response) {
   console.log('Routing request for ' + pathname);
 
-  figlet(pathname, (err, data) => {
+  if (pathname === '/') {
+    var message = 'Welcome!';
+  } else if (pathname === '/hello') {
+    var message = 'Hello world';
+  } else if (pathname === '/bye') {
+    message = 'Goodbye world';
+  } else {
+    message = pathname;
+  }
+
+  figlet(message, (err, data) => {
     if (err) {
       console.log('You got an error: ', err);
     }
     response.write(data);
     response.end();
+
   });
 }
 
