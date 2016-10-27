ce65ed9d428cde56627f93b7a3997025e193aeb6
DrewStock <andrewdstock@gmail.com>
2016-10-26 17:53:45 -0700

    part way there

--- hello_goodbye.js
+++ hello_goodbye.js
@@ -1,19 +1,37 @@
-var http = require('http');
-var url = require('url');
+const http = require('http');
+const url = require('url');
+// const parseUrl = require('url').parse;
 
-const port = 5000;
+const qs = require('querystring');
 
-http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
+const query = qs.stringify({ foo: 'bar'});
 
-    if( url.parse(request.url).pathname == '/hello' ) {
+// console.log(typeof query);
+
+const server = http.createServer(function (request, response) {
+
+    console.log(request.method);
+
+    if ( url.parse(request.url).pathname == '/hello' ) {
+        response.statusCode = 200;
         response.write('hello world');
-    } else if (url.parse(request.url).pathname == '/goodbye' ) {
+    }  else if (url.parse(request.url).pathname == '/goodbye' ) {
+        response.statusCode = 200;
         response.write('goodbye world');
+    }  else if (url.parse(request.url).query == query ) {
+        response.statusCode = 200;
+        response.write('foo bar!');
+    }  else if (url.parse(request.url).pathname == '/wtf' ) {
+        response.statusCode = 404;
+        response.write('page not found!');
+    }   else if (something) {
+
+    }   else {
+        response.statusCode = 404;
+        response.write('page not found');
     }
 
     response.end();
-}).listen(port, err => {
-    if(err) console.log('ERROR!', err);
-    else console.log('http server listening on port', port);
 });
+
+module.exports = server;
---
+++ index.js
@@ -0,0 +1,8 @@
+const server = require('./hello_goodbye');
+
+const port = 5000;
+
+server.listen(port, err => {
+    if(err) console.log('ERROR!', err);
+    else console.log('http server listening on port', port);
+});
