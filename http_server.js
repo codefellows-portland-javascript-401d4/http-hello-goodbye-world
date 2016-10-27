e10d5d9d6c948113a196610eea4d6205dca164da
Gloria Guy <totallygloria@gmail.com>
2016-10-26 17:53:23 -0700

    Added another conditional to see if the http very was a POST request; if so, and if one the /post route, it passes the data off to do some ridiculous post handling.

--- http_server.js
+++ http_server.js
@@ -5,6 +5,7 @@ const urlParser = require('url').parse;
 const querystring = require('querystring');
 const route = require('./router').route;
 const query = require('./router').query;
+const poster = require('./router').poster;
 
 function onRequest(request, response) {
   var url = urlParser(request.url);
@@ -12,7 +13,10 @@ function onRequest(request, response) {
   var qs = querystring.parse(url.query);
   response.setHeader('Content-Type', 'text/plain');
 
-  if (url.query) {
+  if (pathname === '/post' && request.method === 'POST') {
+    console.log('Posting data to /post');
+    poster(pathname, request, response)
+  } else if (url.query) {
     console.log('Query for ', qs);
     query(qs, pathname, response)
   } else {
