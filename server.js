ee3f48061d27d33e4abb2fbbd1b44235e409aaf9
TomTimmel <tom.t.timmel@gmail.com>
2016-10-28 08:06:43 -0700

    added README for resubmittal

--- server.js
+++ server.js
@@ -5,22 +5,22 @@ const aboutHtml = fs.createReadStream('about.html');
 
 
 const server = http.createServer((req, res) =>{
-    console.log(req.url);
-    res.StatusCode = 200;
-    if (req.url === '/') {
-        indexHtml.pipe(res);
-    }
-    else if (req.url === '/about'){
-        aboutHtml.pipe(res);
-    }
-    else {
-        res.write(`Hello World, you asked for ${req.url}`);
-        res.end();
-    }
+  console.log(req.url);
+  res.StatusCode = 200;
+  if (req.url === '/') {
+    indexHtml.pipe(res);
+  }
+  else if (req.url === '/about'){
+    aboutHtml.pipe(res);
+  }
+  else {
+    res.write(`Hello World, you asked for ${req.url}`);
+    res.end();
+  }
 });
 
 const port = 8080;
 server.listen(port, err => {
-    if(err) console.log('Error', err);
-    else console.log('http server listening on port', port);
+  if(err) console.log('Error', err);
+  else console.log('http server listening on port', port);
 });
