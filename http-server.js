5a8d3140b1a4167c66c45a250c670189db6e6e18
caraldi <araldicm@Caitlin-M-Araldi.local>
2016-10-30 21:47:09 -0700

    Added figlet; two url responses

--- http-server.js
+++ http-server.js
@@ -1,22 +1,37 @@
+'use strict';
+
 const http = require('http');
 const fs = require('fs');
+const parseUrl = require('url').parse;
+const query = require('querystring');
+const figlet = require('figlet');
 
-const server = http.createServer((req, res) => {
-    console.log(req.url);
-    res.statusCode = 200;
+const server = http.createServer((request, response) => {
+    response.statusCode = 200;
+    const url = parseUrl(request.url);
 
-    if (req.url === '/') {
-        res.write('hello, world!');
-        res.end();
+    if (request.url === '/') {
+        const hello = 'hello, world!';
+        figlet(hello, {
+            font: 'isometric2'
+        }, (error, data) => {
+         response.end(data);
+      });
     } 
-    else {
-        res.write('goodbye, world!');
-        res.end();
+    else if (request.url === '/blue-heeler') {
+        const acd = 'an alert, curious, & pleasant herding dog';
+        figlet(acd, {
+            font: 'stampatello'
+        }, (error, data) => {
+         response.end(data);
+      });
     }
 });
 
 const port = 8080;
-server.listen(port, err => {
-    if (err) console.log(`ERROR: ${err}`);
-    else console.log(`http server is listening on port ${port}`);
+server.listen(port, error => {
+    if (error) console.log('ERROR', `${error}`);
+    else console.log(`http-server is listening on port ${port}`);
 });
+
+module.exports = server;
