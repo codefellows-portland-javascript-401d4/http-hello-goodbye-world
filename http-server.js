473632081bddf1df642aca215631128c6df2c8fa
caraldi <araldicm@Caitlin-M-Araldi.local>
2016-10-30 22:30:18 -0700

    At least three responses by url, qs & http verb

--- http-server.js
+++ http-server.js
@@ -2,8 +2,8 @@
 
 const http = require('http');
 const fs = require('fs');
-const parseUrl = require('url').parse;
-const query = require('querystring');
+const parseUrl = require('url').parse; 
+const qs = require('querystring');
 const figlet = require('figlet');
 
 const server = http.createServer((request, response) => {
@@ -11,20 +11,49 @@ const server = http.createServer((request, response) => {
     const url = parseUrl(request.url);
 
     if (request.url === '/') {
-        const hello = 'hello, world!';
+        const hello = 'hello, \n world';
         figlet(hello, {
-            font: 'isometric2'
+            font: 'isometric2',
+            horizontalLayout: 'default',
+            verticalLayout: 'default'
         }, (error, data) => {
          response.end(data);
       });
     } 
-    else if (request.url === '/blue-heeler') {
-        const acd = 'an alert, curious, & pleasant herding dog';
+    else if (request.url === '/blue-heeler?type=dog&breed=acd') {
+        console.log(qs.parse('/blue-heeler?type=dog&breed=acd'));
+        const acd = 'australian \n cattle dog';
         figlet(acd, {
-            font: 'stampatello'
+            font: 'avatar',
+            horizontalLayout: 'default',
+            verticalLayout: 'default'
         }, (error, data) => {
          response.end(data);
       });
+    }
+    else if (request.url === '/get' && request.method === 'GET') {
+        const akcSays = 'an alert, curious, & pleasant herding dog';
+        figlet(akcSays, {
+            font: 'stampatello',
+            horizontalLayout: 'default',
+            verticalLayout: 'default'
+        }, (error, data) => {
+         response.end(data);
+      });
+    }
+    else if(request.url === '/end') {
+        const goodbye = 'goodbye, \n world';
+        figlet(goodbye, {
+            font: 'isometric2',
+            horizontalLayout: 'default',
+            verticalLayout: 'default'
+        }, (error, data) => {
+         response.end(data);
+      });
+    }
+    else {
+        response.statusCode = 400;
+        response.end('ERROR: PAGE NOT FOUND');
     }
 });
 
