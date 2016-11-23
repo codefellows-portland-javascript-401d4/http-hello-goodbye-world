8bf4ac5bcc829f8c37342d241abc1116e2521ab2
Nathan P. Keene <npk@txtincorporated.com>
2016-11-23 15:57:38 -0800

    Commit with test failing due to its success

--- test/reqHandlers.test.js
+++ test/reqHandlers.test.js
@@ -5,16 +5,33 @@ chai.use(chaiHttp);
 
 const server = require('../lib/hello-server');
 
-describe('Test output for page routing in hello-goodbye app', (req, res) => {
+describe('Test output for page routing in hello-goodbye app', () => {
 
   const request = chai.request(server);
 
+  //Test fails properly and logs expected value.
   it('logs out response', (done) => {
+    setTimeout(done, 10000);
     request
-      .get('/hello')
-      .then(() => {
-        console.log('res: ', res);
-        assert.deepEqual(res.text, 'foo');
+      .get('/')
+      .then(res => {
+        let resText = res.text;
+        console.log('tested:\n', resText.substr(0, 188));
+        assert.equal(resText.substr(184, 1), '9');
+      })
+      .catch(done);
+  });
+  
+  //Test times out on match.
+  it('logs out response', (done) => {
+    setTimeout(done, 10000);
+    request
+      .get('/')
+      .then(res => {
+        let resText = res.text;
+        console.log('tested:\n', resText.substr(0, 188));
+        let tesText = '8';
+        assert.equal(resText.substr(184, 1), '8');
       })
       .catch(done);
   });
