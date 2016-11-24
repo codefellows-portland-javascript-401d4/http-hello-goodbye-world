b28f61c4e0e4af8242e775e30f070d0b67f22e99
Nathan P. Keene <npk@txtincorporated.com>
2016-11-23 17:14:35 -0800

    Add additional tests for querying and 404s plus helpful console mssgs

--- test/reqHandlers.test.js
+++ test/reqHandlers.test.js
@@ -9,13 +9,37 @@ describe('Test output for page routing in hello-goodbye app', () => {
 
   const request = chai.request(server);
   
-  it('TEST 2 - Sampled figlet string matches test swatch', (done) => {
-    setTimeout(done, 1500);
+  it('Sampled figlet string matches test swatch - default route', (done) => {
     request
       .get('/')
       .then(res => {
         let resText = res.text;
+        console.log('resText.substr(184, 1): ', resText.substr(184, 1));
         assert.equal(resText.substr(184, 1), '8');
+        done();
+      })
+      .catch(done);
+  });
+  
+  it('Sampled figlet string matches test swatch - font-selection', (done) => {
+    request
+      .get('/goodbye?Broadway')
+      .then(res => {
+        let resText = res.text;
+        console.log('resText.substr(484, 1): ', resText.substr(484, 1));
+        assert.equal(resText.substr(484, 1), '8');
+        done();
+      })
+      .catch(done);
+  });
+  
+  it('Sampled figlet string matches test swatch - error-handling', (done) => {
+    request
+      .get('/aloha')
+      .then(res => {
+        let resText = res.text;
+        console.log('resText.substr(684, 1): ', resText.substr(684, 1));
+        assert.equal(resText.substr(684, 1), '|');
         done();
       })
       .catch(done);
