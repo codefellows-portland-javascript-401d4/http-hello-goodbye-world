7756bc5875d6cba6ddf827843c259340c9c9026b
Nathan P. Keene <npk@txtincorporated.com>
2016-11-23 16:34:49 -0800

    Fixed broken test

--- test/reqHandlers.test.js
+++ test/reqHandlers.test.js
@@ -8,29 +8,15 @@ const server = require('../lib/hello-server');
 describe('Test output for page routing in hello-goodbye app', () => {
 
   const request = chai.request(server);
-
-  //Test should fail with expected value of 8.
-  it('Sampled figlet string matches test swatch', (done) => {
-    setTimeout(done, 10000);
-    request
-      .get('/')
-      .then(res => {
-        let resText = res.text;
-        console.log('TESTING; resText.substr(184, 1): ', resText.substr(184, 1));
-        assert.equal(resText.substr(184, 1), '9');
-      })
-      .catch(done);
-  });
   
-  //Test times out on match.
-  it('Sampled figlet string matches test swatch', (done) => {
-    setTimeout(done, 10000);
+  it('TEST 2 - Sampled figlet string matches test swatch', (done) => {
+    setTimeout(done, 1500);
     request
       .get('/')
       .then(res => {
         let resText = res.text;
-        console.log('TESTING; resText.substr(184, 1): ', resText.substr(184, 1));
         assert.equal(resText.substr(184, 1), '8');
+        done();
       })
       .catch(done);
   });
